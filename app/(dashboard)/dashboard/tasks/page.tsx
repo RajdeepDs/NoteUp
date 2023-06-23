2ab4c8a67e0e13bd6cd0bd0f4bd678937137e3f8
Rajdeep Das <rajdeepds626@gmail.com>
2023-06-23 18:22:52 +0530

    task page added

---
+++ app/(dashboard)/dashboard/tasks/page.tsx
@@ -0,0 +1,7 @@
+export default function TaskPage(){
+    return(
+        <div className="font-bold text-gray-400 flex justify-center items-center text-xl">
+            This feature is not yet implemented. Please be patient.
+        </div>
+    );
+}
