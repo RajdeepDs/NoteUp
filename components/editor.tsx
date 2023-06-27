500128e52588f7724ae16b1942147d24c7e29735
Rajdeep Das <rajdeepds626@gmail.com>
2023-06-27 20:05:18 +0530

    tag-ui changes

--- components/editor.tsx
+++ components/editor.tsx
@@ -147,10 +147,10 @@ export function Editor({ note }: EditorProps) {
               {note?.tags?.map((tag) => (
                 <li
                   key={tag.id}
-                  className="rounded-sm bg-accent-2 p-1 text-xs text-accent-5 "
+                  className="rounded-sm bg-accent-1 p-1 text-xs text-accent-5 "
                 >
                   {tag.name}
-                  <button className="ml-1 p-1 hover:bg-accent-3/50">x</button>
+                  <button className="ml-1 p-1 hover:bg-accent-2/50">x</button>
                 </li>
               ))}
               <AddTag addtags={addtags} />
