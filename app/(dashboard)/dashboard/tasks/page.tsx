dc1bb6d6ef2e2cfe5e9500cf5afeb9522a3748e8
Rajdeep Das <rajdeepds626@gmail.com>
2023-07-22 18:49:33 +0530

    task page header added

--- app/(dashboard)/dashboard/tasks/page.tsx
+++ app/(dashboard)/dashboard/tasks/page.tsx
@@ -1,7 +1,19 @@
-export default function TaskPage(){
-    return(
-        <div className="font-bold text-gray-400 flex justify-center items-center text-xl">
-            This feature is not yet implemented. Please be patient.
+import DashboardHeader from "@/components/header";
+
+export default function TaskPage() {
+  return (
+    <div>
+      <div className="mt-2 border-b">
+        <DashboardHeader
+          title="Tasks"
+          description="Manage your tasks and get things done."
+        />
+      </div>
+      <div className="mt-4">
+        <div className="flex items-center justify-center text-xl font-bold text-gray-400">
+          This feature is not yet implemented. Please be patient.
         </div>
-    );
+      </div>
+    </div>
+  );
 }
