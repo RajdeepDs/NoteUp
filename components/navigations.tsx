71c03f278ca2bb38b73ad9b5a55aad975d49b9f5
Rajdeep Das <rajdeepds626@gmail.com>
2023-08-10 11:36:43 +0530

    fix: navigation button not working

--- components/navigations.tsx
+++ components/navigations.tsx
@@ -2,34 +2,28 @@ import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "./ui/button";
-import { getCurrentUser } from "@/lib/session";
 
-export default async function Navigations() {
-  const userExists = await getCurrentUser();
-  console.log(userExists);
-  
+export default function Navigations({ user }: any) {
   return (
-    <>
-      <div className="flex items-center space-x-4">
-        <Link
-          href="#about"
-          className="hidden text-accent-3 hover:text-primary md:flex"
-        >
-          About
-        </Link>
-        <Link
-          href="#feature"
-          className="hidden text-accent-3 hover:text-primary md:flex"
-        >
-          Feature
-        </Link>
-        <Link
-          className={cn(buttonVariants({ variant: "ghost" }))}
-          href={userExists ? "/dashboard" : "/login"}
-        >
-          {userExists ? "Dashboard" : "Sign Up"}
-        </Link>
-      </div>
-    </>
+    <div className="flex items-center space-x-4">
+      <Link
+        href="#about"
+        className="hidden text-accent-3 hover:text-primary md:flex"
+      >
+        About
+      </Link>
+      <Link
+        href="#feature"
+        className="hidden text-accent-3 hover:text-primary md:flex"
+      >
+        Feature
+      </Link>
+      <Link
+        className={cn(buttonVariants({ variant: "outline" }))}
+        href={user ? "/dashboard" : "/login"}
+      >
+        {user ? "Dashboard" : "Sign Up"}
+      </Link>
+    </div>
   );
 }
